d55ba5f78202f642f6a26b087cccc110ec5f409d
akash <96342895+akashreddy5443123@users.noreply.github.com>
2025-06-27 08:30:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify, send_file
 import pymysql
 pymysql.install_as_MySQLdb()
-from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 import bcrypt
